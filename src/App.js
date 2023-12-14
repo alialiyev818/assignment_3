f53d3ef09bce2d04a56fa7d0c94f454f92f327e9
Ali <superguy@nobody.com>
2023-12-14 23:23:42 +0400

    Version 2 - Decluttered the React files and components. Started working on the homepage.
    It's important to note that the final version will be very different because I'm still trying to figure out exactly I will do.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
+  const projects = [
+    { id: 1, title: 'Project One', description: 'Description for Project One' },
+    { id: 2, title: 'Project Two', description: 'Description for Project Two' },
+    
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My Homepage</h1>
       </header>
+      <section className="Projects">
+        <h2>My Projects</h2>
+        <ul>
+          {projects.map(project => (
+            <li key={project.id}>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
