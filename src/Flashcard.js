335c73f01a744951a4ad2dac2b6c6dbe117422c1
Ali <superguy@nobody.com>
2023-12-20 20:41:22 +0400

    Version 8 - Added the "hover to view the edit and delete buttons" functionality to the Flashcard component. Tomorrow I will make those buttons work by making a new component.

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
 
-function Flashcard({ card }) {
+function Flashcard({ card, onDelete, onEdit }) {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
     setIsFlipped(!isFlipped);
   };
 
   return (
-    <div onClick={handleClick}>
+    <div 
+      onClick={handleClick} 
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       {isFlipped ? card.answer : card.question}
       <div>Last Modified: {card.lastModified.toLocaleString()}</div>
       <div>Status: {card.status}</div>
+      {isHovered && (
+        <div>
+          <button onClick={() => onEdit(card)}>Edit</button>
+          <button onClick={() => onDelete(card)}>Delete</button>
+        </div>
+      )}
     </div>
   );
 }
