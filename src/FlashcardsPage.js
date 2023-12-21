48fc851306fc5fb83b79b77804f52c22b121bbca
Ali <superguy@nobody.com>
2023-12-21 23:57:19 +0400

    Version 9 - Now the delete button works completely. Regarding the Edit function, even though provides the interface to edit and save your changes, the save button doesn't work for now. Will work on it later.

--- src/FlashcardsPage.js
+++ src/FlashcardsPage.js
@@ -10,11 +10,22 @@ function FlashcardsPage() {
     { question: 'What am I?', answer: 'a Human', lastModified: new Date(), status: 'Want to Learn' },
   ]);
 
+  const handleDelete = (cardToDelete) => {
+    setFlashcards(flashcards.filter(card => card !== cardToDelete));
+  };
+
+  const handleEdit = (cardToEdit) => {};
+
   return (
     <div>
       <h1>Flashcards Page</h1>
       {flashcards.map((card, index) => (
-        <Flashcard key={index} card={card} />
+        <Flashcard 
+          key={index} 
+          card={card} 
+          onDelete={handleDelete} 
+          onEdit={handleEdit}
+        />
       ))}
     </div>
   );
