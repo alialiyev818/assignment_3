d37832eb004d5376b613f6a0e09ef56818d2ca9a
Ali <superguy@nobody.com>
2023-12-22 22:57:46 +0400

    Version 10 - Started working on the styling of the webpage and completely modernized the contact me page.

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import './ContactPage.css'; 
 
 function ContactPage() {
   return (
-    <div>
+    <div className="contact-container">
       <h1>Contact Me</h1>
-      <form>
-        <input type="text" placeholder="Subject" /><br />
-        <input type="email" placeholder="Email" /><br />
-        <textarea placeholder="Your Message"></textarea><br />
+      <form className="contact-form">
+        <div className="form-group">
+          <label htmlFor="subject">Subject</label>
+          <input type="text" id="subject" placeholder="Subject" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Your Message</label>
+          <textarea id="message" placeholder="Your Message"></textarea>
+        </div>
         <button type="submit">Send</button>
       </form>
     </div>
