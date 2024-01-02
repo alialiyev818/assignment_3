31d241ee237d1ac7e8808547ea93029dc39c20e6
Ali <superguy@nobody.com>
2024-01-03 03:11:13 +0400

    Version 21.1 - Finally fixed and done with the share function!
    Now users can select multiple cards by clicking on the bar under each card which turns the borders and the specified bar to green to let the users know that the card(s) is selected.
    
    Clicking on share after selecting multiple cards will send their details over email in JSON format.
    
    Turns out that even though I have written css code for the bar under "checkbox-container" class (in flashcarditem.css), I have forgetten to add the specified class to the FlashCardItem component.
    
    I also added the necessary code (in FlashCardItem.jsx) to see the Card Status and Last Modification Date of the cards on the front side of it.

--- src/components/FlashCardItem.jsx
+++ src/components/FlashCardItem.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import "../assets/style/components/flashcarditem.css";
 
-const FlashCardItem = ({ card, onDelete, onUpdate, onFlip }) => {
+const FlashCardItem = ({ card, onDelete, onUpdate, onSelect, onFlip, isSelected }) => {
     return (
         <div
-            className={`flashcard-item ${card.isFlipped ? "flipped" : ""}`}
+            className={`flashcard-item ${isSelected ? "selected" : ""} ${
+                card.isFlipped ? "flipped" : ""
+            }`}
             onClick={() => onFlip(card.id)}
         >
+            <div className="checkbox-container">
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    checked={isSelected}
+                    onChange={() => onSelect(card.id)}
+                />
+            </div>
             <div className="flipperSide">
                 <div className="front">
+                    <p className="status">Card Status: {card.status}</p>
                     {card.image ? (
                         <img src={card.image} alt="Card" className="card-image" />
                     ) : (
                         <h3 className="frontTxt">{card.frontText}</h3>
                     )}
+                    <span
+                        className="hidden"
+                        data-last-modification={card.lastModificationDateTime}
+                    ></span>
+                    <p className="last-modification">
+                        Last Modified: {card.lastModificationDateTime}
+                    </p>
                     <div className="allButtons">
                         <button className="update-btn" onClick={() => onUpdate(card)}>
                             Update
